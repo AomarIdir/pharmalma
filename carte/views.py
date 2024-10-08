d9cc8956ae7bb6b543e47d550b3207e09db3a7cd
LOICK GOURVIL <loick.gourvil@laposte.fr>
2024-10-08 16:32:55 +0200

    passage des points a afficher par l'url

--- carte/views.py
+++ carte/views.py
@@ -1,21 +1,22 @@
 from django.shortcuts import render
 import json
 
+#working url : 
+#http://127.0.0.1:8000/carte/?ville=suc%C3%A9+sur+erdre&points=[{"lat":47.344062,"lng":-1.5289619},{"lat":47.341824,"lng":-1.526767}]
 def carte_view(request):
     ville = request.GET.get('ville')
     message = None
-    #points_str = request.GET.get('points')
+    points_str = request.GET.get('points')
 
     if not ville:
         message = "Veuillez entrer une ville"
     
-    points = [{'lat': 47.344062, 'lng': -1.5289619}, {'lat': 47.341824, 'lng': -1.526767}]
-#   if points_str:
-#       try:
-#           points = json.loads(points_str) 
-#       except json.JSONDecodeError:
-#           points = []
-#           message = "Format de points invalide"
+    if points_str:
+        try:
+            points = json.loads(points_str) 
+        except json.JSONDecodeError:
+            points = []
+            message = "Format de points invalide"
 
     context = {
         'ville': ville,
